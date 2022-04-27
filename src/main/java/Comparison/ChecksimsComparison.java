d6f0a735d8e7c9af942f837e08872c2bb0a206e2
molson18 <molson18@punahou.edu>
2022-04-27 18:19:16 -0400

    changed it back to line comparison

--- src/main/java/Comparison/ChecksimsComparison.java
+++ src/main/java/Comparison/ChecksimsComparison.java
@@ -27,7 +27,7 @@ public class ChecksimsComparison extends Comparison {
     ChecksimsComparison() {
         this.assignmentMap = new ConcurrentHashMap<>();
         this.algorithm = AlgorithmRegistry.getInstance().getDefaultImplementation();
-        this.algorithm = Checksims.algorithm.smithwaterman.SmithWaterman.getInstance();
+//        this.algorithm = Checksims.algorithm.smithwaterman.SmithWaterman.getInstance();
         this.tokenization = this.algorithm.getDefaultTokenType();
     }
 
